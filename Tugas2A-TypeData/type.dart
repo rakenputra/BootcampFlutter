b1d1bcdfe2754a9caa1c19d10eaa15f283dd6582
raken putra <raken.putra@gmail.com>
2024-10-29 20:52:38 +0700

    Update code

--- Tugas2A-TypeData/type.dart
+++ Tugas2A-TypeData/type.dart
@@ -2,41 +2,43 @@
  
  void main(){
 //soal nomor 1
-//  var word ='Dart';
-//  var second ='is';
-//  var third ='awesome';
-//  var fourth ='and';
-//  var fifth ='i';
-//  var sixth ='love';
-//  var seventh ='it';
+ var word ='Dart';
+ var second ='is';
+ var third ='awesome';
+ var fourth ='and';
+ var fifth ='i';
+ var sixth ='love';
+ var seventh ='it';
 
-//  print (word+' '+second+' '+third+' '+fourth+' '+fifth+' '+sixth+' '+seventh);
+ print (word+' '+second+' '+third+' '+fourth+' '+fifth+' '+sixth+' '+seventh);
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////
  //soal nomor 2
-// var sentence = "I am going to be Flutter Developer";
-// var FirstWord = sentence[0] ;
-// var SecondWord = sentence[2] + sentence[3] ;
-// var ThirdWord = sentence[5] + sentence[6] + sentence[7] + sentence[8] + sentence[9]; 
-// var FourthWord = sentence[11] + sentence[12];
-// var FifthWord = sentence[14] + sentence[15]; 
-// var SixthWord = sentence[17] + sentence[18] + sentence[19] + sentence[20] + sentence[21] + sentence[22] + sentence[23];
-// var SeventhWord = sentence[25] + sentence[26] + sentence[27] + sentence[28] +sentence[29] + sentence[30] + sentence[31] + sentence[32] + sentence[33];
+var sentence = "I am going to be Flutter Developer";
+var FirstWord = sentence[0] ;
+var SecondWord = sentence[2] + sentence[3] ;
+var ThirdWord = sentence[5] + sentence[6] + sentence[7] + sentence[8] + sentence[9]; 
+var FourthWord = sentence[11] + sentence[12];
+var FifthWord = sentence[14] + sentence[15]; 
+var SixthWord = sentence[17] + sentence[18] + sentence[19] + sentence[20] + sentence[21] + sentence[22] + sentence[23];
+var SeventhWord = sentence[25] + sentence[26] + sentence[27] + sentence[28] +sentence[29] + sentence[30] + sentence[31] + sentence[32] + sentence[33];
  
-// print ("First Word : " +FirstWord);
-// print ("Second Word : " +SecondWord);
-// print ("Third Word : " +ThirdWord);
-// print ("Fourth Word : " +FourthWord);
-// print ("Fifth Word : " +FifthWord);
-// print ("Sixth Word : " +SixthWord);
-// print ("Seventh Word : " +SeventhWord);
+print ("First Word : " +FirstWord);
+print ("Second Word : " +SecondWord);
+print ("Third Word : " +ThirdWord);
+print ("Fourth Word : " +FourthWord);
+print ("Fifth Word : " +FifthWord);
+print ("Sixth Word : " +SixthWord);
+print ("Seventh Word : " +SeventhWord);
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //soal nomor 3
-// print ('masukkan nama depan :');
-// String? namadepan = stdin.readLineSync()!;
-// print('masukkan nama belakang :');
-// String? namabelakang = stdin.readLineSync()!;
-// print('nama lengkap anda adalah : $namadepan $namabelakang');
-
+print ('masukkan nama depan :');
+String? namadepan = stdin.readLineSync()!;
+print('masukkan nama belakang :');
+String? namabelakang = stdin.readLineSync()!;
+print('nama lengkap anda adalah : $namadepan $namabelakang');
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //soal nomor 4
 var a = 5;
 var b = 10;
